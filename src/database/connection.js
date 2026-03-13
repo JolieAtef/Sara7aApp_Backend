d570cdfc47cdc66416e7438b21f16e72f900ac51
JolieAtef <Jolie.Atef.25@gmail.com>
2026-03-13 17:54:26 +0200

    edit connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,8 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const db_url = process.env.DB_URL;
+if (!db_url) {
+    throw new Error("DB_URL is missing! Make sure it's set in .env or Vercel environment variables.");
+  }
+  
 export const databaseConnection =()=>{
-    mongoose.connect(db_url).then(()=>{
+    mongoose.connect(db_url ,{ useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
         console.log("database connected")
     }).catch((err)=>{
         console.log(err)
