754ff01121de5ebb14fcdc182827b0a9ae7ba503
JolieAtef <Jolie.Atef.25@gmail.com>
2026-03-13 18:06:36 +0200

    debug-env-variables

--- src/app.controller.js
+++ src/app.controller.js
@@ -3,9 +3,12 @@ import { databaseConnection } from "./database/connection.js"
 import authRouter from "./modules/auth/auth.controller.js"
 import userRouter from "./modules/user/user.controller.js"
 import messageRouter from "./modules/message/message.controller.js"
+import dotenv from "dotenv";
+
 
 export const bootstrap =()=>{
-   
+    
+    dotenv.config();
     const app = express()
     app.use(express.json())
     databaseConnection()
@@ -15,6 +18,13 @@ export const bootstrap =()=>{
     
     app.use(express.urlencoded({extended:true}))
     app.use("/uploads", express.static("uploads"))
+
+
+    app.get("/debug-env", (req, res) => {
+        res.json({
+          DB_URL: process.env.DB_URL ? "SET" : "UNDEFINED",
+        });
+      });
 
 
     app.listen(3000,()=>{
--- src/database/connection.js
+++ src/database/connection.js
@@ -3,12 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const db_url = process.env.DB_URL;
-if (!db_url) {
-    throw new Error("DB_URL is missing! Make sure it's set in .env or Vercel environment variables.");
-  }
-  
+
 export const databaseConnection =()=>{
-    mongoose.connect(db_url ,{ useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
+    mongoose.connect(db_url).then(()=>{
         console.log("database connected")
     }).catch((err)=>{
         console.log(err)
