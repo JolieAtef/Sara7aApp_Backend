d8fa18387b402d7e96c53574e579fd80e8410d75
JolieAtef <Jolie.Atef.25@gmail.com>
2026-03-13 18:39:05 +0200

    test again

--- src/app.controller.js
+++ src/app.controller.js
@@ -3,29 +3,21 @@ import { databaseConnection } from "./database/connection.js"
 import authRouter from "./modules/auth/auth.controller.js"
 import userRouter from "./modules/user/user.controller.js"
 import messageRouter from "./modules/message/message.controller.js"
-import dotenv from "dotenv";
+
 
 
 export const bootstrap =()=>{
     
-    dotenv.config();
     const app = express()
     app.use(express.json())
-    app.get("/debug-env", (req, res) => {
-        res.json({
-          DB_URL: process.env.DB_URL ? "SET" : "UNDEFINED",
-        });
-      });
 
-    // databaseConnection()
+    databaseConnection()
     app.use("/auth",authRouter)
     app.use("/user",userRouter)
     app.use("/message",messageRouter)
     
     app.use(express.urlencoded({extended:true}))
     app.use("/uploads", express.static("uploads"))
-
-
 
 
     app.listen(3000,()=>{
